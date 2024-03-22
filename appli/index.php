7b0155c28de94f2cf3d09f1bb65652bf1b0286d5
chaima garoui <chaimagaroui2@gmail.com>
2024-03-22 09:35:53 +0100

    page index.php ok avec un premier affichage

---
+++ appli/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Document</title>
+    </head>
+    <body>
+
+        <h1>Ajouter un produit</h1>
+        <form action= "traitement.php" method="post">
+            <p>
+                <label >
+                    Nom du produit :
+                    <input type="text" name="name">
+
+                </label>
+            </p>
+            <p>
+                <label >
+                    Prix du produit :
+                    <input type="number" step="any" name="price">
+
+                </label>
+            </p>
+            <p>
+                <label >
+                    Quantité desirée :
+                    <input type="number" name="qtt" value="1">
+
+                </label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="ajouter le produit">
+            </p>
+        
+        </form>
+    </body>
+</html>
