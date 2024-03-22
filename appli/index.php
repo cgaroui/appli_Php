701fb8f41dba2e4f10379179b77554055c62a1fd
chaima garoui <chaimagaroui2@gmail.com>
2024-03-22 17:03:24 +0100

    button panier ramene sur page recap ok et mise en page index  pk

--- appli/index.php
+++ appli/index.php
@@ -3,38 +3,48 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
         <title>Document</title>
     </head>
     <body>
-
-        <h1>Ajouter un produit</h1>
-        <form action= "traitement.php" method="post">
-            <p>
-                <label >
-                    Nom du produit :
-                    <input type="text" name="name">
-
-                </label>
-            </p>
-            <p>
-                <label >
-                    Prix du produit :
-                    <input type="number" step="any" name="price">
-
-                </label>
-            </p>
-            <p>
-                <label >
-                    Quantité desirée :
-                    <input type="number" name="qtt" value="1">
-
-                </label>
-            </p>
-            <p>
-                <input type="submit" name="submit" value="ajouter le produit">
-            </p>
+        <div class="d-grid gap-2 col-6 mx-auto">
         
+            <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
+            <a href="recap.php" class="btn btn-light" type="submit"  role="button" >Panier</a>
+
+            <h1>Ajouter un produit</h1>
+        </div>
+        
+
+        <div class="d-grid gap-2 col-6 mx-auto">
+        
+            <form action= "traitement.php" method="post">           <!-- Utilisation de la méthode POST pour envoyer les données du formulaire au serveur -->
+            
+                <label >
+                    Nom du produit : <br>
+                    <input style="width: 300px;" type="text" name="name">
+
+                </label>
+            
+                <label >
+                    Prix du produit : <br>
+                    <input style="width: 300px;"type="number" step="any" name="price">
+
+                </label>
+            
+                <label >
+                    Quantité desirée : <br>
+                    <input style="width: 300px;" type="number" name="qtt" value="1">
+
+                </label>
+            
+            
+        
+                <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
+            
+            </div>
+           
         </form>
     </body>
 </html>
