6afdfe96f3779759408a2f7911729bc29e8bf221
chaima garoui <chaimagaroui2@gmail.com>
2024-03-22 12:01:12 +0100

    recap.php c'est ok

---
+++ appli/Recap.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Récapitulatif des produits</title>
+    </head>
+    <body>
+        <?php
+            if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+                echo "<p>Aucun produit en session..</p>";
+            }
+            else{
+                echo "<table>",                     //initialisation du tableau HTML
+                        "<thead>",
+                            "<tr>",
+                                "<th>#</th>",
+                                "<th>Nom</th>",
+                                "<th>Prix</th>",
+                                "<th>Quantité</th>",
+                                "<th>Total</th>",
+                            "</tr>",
+                        
+                        "</thead>",
+                        "<tbody>"; 
+                $totalGeneral = 0;
+                foreach($_SESSION['products'] as $index => $product){  //permet d'excuter produit par produit, les mêmes instructions qui vont permettre l'affichage uniforme de chacun d'entre eux.
+                    echo "<tr>",
+                            "<td>".$index."</td>",
+                            "<td>".$product['name']."</td>",
+                            "<td>".$product['price']."</td>",
+                            "<td>".$product['qtt']."</td>",
+                            "<td>".$product['total']."</td>",
+                           
+                        "</tr>";
+                    $totalGeneral+=$product['total'];
+                }      
+                echo "<tr>",
+                        "<td colspan=4>Total Genéral : </td>",
+                        "<td><strong>".number_format($totalGeneral, 2, "," , "&nbsp;")."&nbsp;€</strong></td>",
+                    "</tr>",
+                    "</tbody>",
+                "</table>";
+            }
+        
+        
+        ?>
+        
+    </body>
+</html>
--- appli/Traitement.php
+++ appli/Traitement.php
@@ -1,20 +1,21 @@
 <?php
 
     session_start();
-    if(isset($_post['submit'])){
+    if(isset($_POST['submit'])){
 
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $price = filter_input(INPUT_POST,"price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $name = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
-        if($name && $price &&$qtt){
+        if($name && $price && $qtt){
+
             $product = [
                 "name"=>$name,
                 "price"=>$price,
                 "qtt"  =>$qtt,
                 "total"=>$price*$qtt
             ];
-            $_session['products'][] =$product;
+            $_SESSION['products'][] = $product;
 
         }
 
