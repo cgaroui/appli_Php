e78a787a1e063f102dda19071d1ac8cfb413f90f
chaima garoui <chaimagaroui2@gmail.com>
2024-04-02 11:30:53 +0200

    correction indentations du code

--- appli/Recap.php
+++ appli/Recap.php
@@ -1,83 +1,71 @@
 <?php
   session_start();
    
-?>
 
-
+    if(!isset($_SESSION['products']) || empty($_SESSION['products'])){ //isset pour verifier qu'aucun produit n'existe et le empty c'est pour verifier que la session product est vide 
+        echo "<p>Aucun produit en session..</p>";
+    }
+    else{
+        echo "<div class='table-responsive'>",
+                "<table  class='table table-bordered border-primary'>",                     //initialisation du tableau HTML
+                    "<thead>",
+                        "<tr class='table-primary'>",
+                            "<th>#</th>",
+                            "<th>Nom</th>",
+                            "<th>Prix</th>",
+                            "<th>Quantité</th>",
+                            "<th>Total</th>",   
+                        "</tr>",
+                    
+                    "</thead>",
+                    "<tbody>", 
+            "</div>";
+    }
+        $totalGeneral = 0;
+        $nbProduits =0;
         
 
+        foreach($_SESSION['products'] as $index => $product){  //permet d'excuter produit par produit, les mêmes instructions qui vont permettre l'affichage uniforme de chacun d'entre eux.
+        
+            $prixToalProduit = $product['price']*$product['qtt'] ;  //calcul le prix total d'un SEUL produit en fonction de la quantité qu'on choisi
+            echo "<tr>" .
+                    "<td>" . $index . "</td>" .
+                    "<td>" . $product['name'] . "</td>" .
+                    "<td>" . $product['price'] . "</td>" .
+                    "<td>" . $product['qtt'] .
+                        "&nbsp;&nbsp;"
+                        ."<a id=qtt href='traitement.php?action=ajouterQtt&id=$index' class='btn btn-info'>+</a>" . "&nbsp;&nbsp;".
+                            "<a id=qtt href='traitement.php?action=diminuerQtt&id=$index' class='btn btn-secondary'>-</a>" .
+                    "</td>" .
+                    "<td>".$prixToalProduit."</td>",
 
-        <?php
-            if(!isset($_SESSION['products']) || empty($_SESSION['products'])){ //isset pour verifier qu'aucun produit n'existe et le empty c'est pour verifier que la session product est vide 
-                echo "<p>Aucun produit en session..</p>";
-            }
-            else{
-                echo "<div class='table-responsive'>",
-                        "<table  class='table table-bordered border-primary'>",                     //initialisation du tableau HTML
-                            "<thead>",
-                                "<tr class='table-primary'>",
-                                    "<th>#</th>",
-                                    "<th>Nom</th>",
-                                    "<th>Prix</th>",
-                                    "<th>Quantité</th>",
-                                    "<th>Total</th>",   
-                                "</tr>",
-                            
-                            "</thead>",
-                            "<tbody>", 
-                    "</div>";
-            }
-                $totalGeneral = 0;
-                $nbProduits =0;
-               
-
-                foreach($_SESSION['products'] as $index => $product){  //permet d'excuter produit par produit, les mêmes instructions qui vont permettre l'affichage uniforme de chacun d'entre eux.
+    
+                    "</tr>";
+                    $nbProduits +=$product['qtt'];             //calcul la quantité d'elements total dans le panier (additionne la qtt de tous les produits dans le panier) 
+                    $totalGeneral += $product['price']*$product['qtt'];        //calcul le prix total (additionne le prix de tous les produits dans le panier)
                 
-                    $prixToalProduit = $product['price']*$product['qtt'] ;  //calcul le prix total d'un SEUL produit en fonction de la quantité qu'on choisi
-                    echo "<tr>" .
-                            "<td>" . $index . "</td>" .
-                            "<td>" . $product['name'] . "</td>" .
-                            "<td>" . $product['price'] . "</td>" .
-                            "<td>" . $product['qtt'] .
-                                "&nbsp;&nbsp;"
-                                ."<a id=qtt href='traitement.php?action=ajouterQtt&id=$index' class='btn btn-info'>+</a>" . "&nbsp;&nbsp;".
-                                    "<a id=qtt href='traitement.php?action=diminuerQtt&id=$index' class='btn btn-secondary'>-</a>" .
-                            "</td>" .
-                            "<td>".$prixToalProduit."</td>",
-
-           
-                         "</tr>";
-                         $nbProduits +=$product['qtt'];             //calcul la quantité d'elements total dans le panier (additionne la qtt de tous les produits dans le panier) 
-                         $totalGeneral += $product['price']*$product['qtt'];        //calcul le prix total (additionne le prix de tous les produits dans le panier)
-                        
-                }
-                        
-                 
+        }
                 
-     
-                echo "<tr>",    
-                        "<td colspan=1>Nombre total de produits : </td>",
-                        "<td><strong> ".number_format($nbProduits,0)." </strong></td>",//pour afficher le resultat de nb broduit sous forme entier avec 0 decimal apres la virgule(quantité)
-                        "<td colspan=2>Total Genéral : </td>.",
-                        "<td><strong>".number_format($totalGeneral, 2, "," , "&nbsp;")."&nbsp;€</strong></td>",//pour afficher le resultat de nb broduit sous forme entier avec 2 decimaux apres la virgule(prix)
-                       
-                    "</tr>",
-                    "</tbody>",
-                "</table>";
-            
-           
-            if(isset($_SESSION['message'])){    //s'il ya un message dans la session 
-                echo $_SESSION['message'];      //on l'affiche 
-                unset($_SESSION['message']);    //ensuite il disparait lorsqu'on raffréchit la page 
-            }
-            
-          
-            
             
         
-        ?>
 
-<?php
+        echo "<tr>",    
+                "<td colspan=1>Nombre total de produits : </td>",
+                "<td><strong> ".number_format($nbProduits,0)." </strong></td>",//pour afficher le resultat de nb broduit sous forme entier avec 0 decimal apres la virgule(quantité)
+                "<td colspan=2>Total Genéral : </td>.",
+                "<td><strong>".number_format($totalGeneral, 2, "," , "&nbsp;")."&nbsp;€</strong></td>",//pour afficher le resultat de nb broduit sous forme entier avec 2 decimaux apres la virgule(prix)
+                
+            "</tr>",
+            "</tbody>",
+        "</table>";
+    
+    
+    if(isset($_SESSION['message'])){    //s'il ya un message dans la session 
+        echo $_SESSION['message'];      //on l'affiche 
+        unset($_SESSION['message']);    //ensuite il disparait lorsqu'on raffréchit la page 
+    }
+    
+
     $content = ob_get_clean();
     $titre = "Récapitulatif des produits";
     require_once "template.php"; 
--- appli/index.php
+++ appli/index.php
@@ -6,55 +6,48 @@ require_once('nbArticles.php');
 //session_start();
 ob_start();
 
-// Affiche  message s'il est présent dans la session
-if (isset($_SESSION['message'])) {
-    echo "<div>" . $_SESSION['message'] . "</div>";
-    unset($_SESSION['message']); 
-}
 ?>
 
-        <div class="d-grid gap-2 col-6 mx-auto">
-        
-            <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
-                <button type="button" class="btn btn-light">
-                    <a href="recap.php">Panier</a>
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                            <?= NombreProduits() ?>
-                            <span class="visually-hidden"></span>
-                        </span>
-                </button>
+<div class="d-grid gap-2 col-6 mx-auto">
 
-            <h1>Ajouter un produit</h1>
-        </div>
-        
+    <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
+        <button type="button" class="btn btn-light">
+            <a href="recap.php">Panier</a>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    <?= NombreProduits() ?>
+                    <span class="visually-hidden"></span>
+                </span>
+        </button>
 
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <form action="traitement.php?action=ajouterProduit" method="post">           <!-- Utilisation de la méthode POST pour envoyer les données du formulaire au serveur -->
-            
-                <label >
-                    Nom du produit : <br>
-                    <input   style="width: 300px;" type="text" name="name">
-                </label>
-            
-                <label >
-                    Prix du produit : <br>
-                    <input min=1 style="width: 300px;"type="number" step="any" name="price">
-                </label>
-            
-                <label >
-                    Quantité desirée : <br>
-                    <input min=1 id="" style="width: 300px;" type="number" name="qtt" value="1">
-                </label>
-            
-            
+    <h1>Ajouter un produit</h1>
+</div>
+
+
+<div class="d-grid gap-2 col-6 mx-auto">
+    <form action="traitement.php?action=ajouterProduit" method="post">           <!-- Utilisation de la méthode POST pour envoyer les données du formulaire au serveur -->
+    
+        <label >
+            Nom du produit : <br>
+            <input   style="width: 300px;" type="text" name="name">
+        </label>
+    
+        <label >
+            Prix du produit : <br>
+            <input min=1 style="width: 300px;"type="number" step="any" name="price">
+        </label>
+    
+        <label >
+            Quantité desirée : <br>
+            <input min=1 id="" style="width: 300px;" type="number" name="qtt" value="1">
+        </label>
+    
+    
+
+        <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
+    </form>
+    
+    </div>
         
-                <input class="btn btn-primary" type="submit" name="submit" value="ajouter le produit">
-            </form>
-           
-         </div>
-        
-    </body>
-</html>
 
 <?php
     $content = ob_get_clean();
